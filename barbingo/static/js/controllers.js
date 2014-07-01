00b06988061defddc83912cc37144f09634e996e
Brian Stearns <brian@ycharts.com>
2014-07-01 01:35:29 -0400

    Shitty frontend, check

--- barbingo/static/js/controllers.js
+++ barbingo/static/js/controllers.js
@@ -1,10 +1,8 @@
 var barBingo = angular.module("barBingo", []);
 
-barBingo.controller('BingoBoardCtrl', function ($scope){
-    $scope.squares = [
-    {'text': "Hi there", "pos": 1},
-    {'text': "Yo there", "pos": 2},
-    {'text': "Hi Yo", "pos": 3}
-    ];
-    
+barBingo.controller('BingoBoardCtrl', function ($scope, $http){
+    $http.get('/api/v0/game/1/card/1/').success(function(data){
+        $scope.squares = data.squares;
+        console.log($scope);
+    });
 });
